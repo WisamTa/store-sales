7ae2b08e994f4f23bc96d58346eeec236b7b97e8
WisamTa <wisamtaqaly@gmail.com>
2021-10-27 08:07:04 +0000

    Fixing break synatx while creating user request loop

--- run.py
+++ run.py
@@ -13,63 +13,28 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('store-sales')
 
 
-def start():
-    """
-    Start menu that the user can choose between 6 different tasks.
-    """
-    print("""
-                --------MENU--------
-                1. Add new item\n\
-                2. View all items\n\
-                3. Delete an item\n\
-                4. Search item\n\
-                5. Reset items\n\
-                6. Exit\n
-                    """)
-
-    while True:
-        choise = input("Choose the number of the task you want to do: \n")
-        if choise == '1':
-            print(" Add new item...\n")
-            add_new_item()
-            break
-        elif choise == '2':
-            print("View all item...\n")
-            show_all_item()
-            break
-        elif choise == '3':
-            print(" Delete an item...\n")
-            print("You have to search for the contact you want to delete\n")
-            search_item()
-            break
-        elif choise == '4':
-            print("Search menu...\n")
-            search_item()
-            break
-        elif choise == '5':
-            print("Reset items...\n")
-            validate_reset()
-            break
-        elif choise == '6':
-            print("Exit programme...")
-            exit_programme()
-            break
-        else:
-            print("Not a valid input please enter a number 1-6")
-
 
 def get_sales_data():
     """
     Get sales figures input from the user.
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Dark Choco and Pistachio, Salted Dark "
+              "Choco, White Choco, Caramel Choco Ship, Peanut, Cranberry")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")
-     
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        data_str = input("Enter your data here: ")
+
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+
+    return sales_data
+
 
 def validate_data(values):
     """
@@ -85,6 +50,9 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
 
 
-get_sales_data()
+data = get_sales_data()
