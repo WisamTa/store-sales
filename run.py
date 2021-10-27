99f631195e43b60fd2a33420eaba8367cafec3ea
WisamTa <wisamtaqaly@gmail.com>
2021-10-27 07:04:35 +0000

    Add The six choises

--- run.py
+++ run.py
@@ -13,6 +13,51 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('store-sales')
 
 
+def start():
+    """
+    Start menu that the user can choose between 6 different tasks.
+    """
+    print("""
+                --------MENU--------
+                1. Add new item\n\
+                2. View all items\n\
+                3. Delete an item\n\
+                4. Search item\n\
+                5. Reset items\n\
+                6. Exit\n
+                    """)
+
+    while True:
+        choise = input("Choose the number of the task you want to do: \n")
+        if choise == '1':
+            print(" Add new item...\n")
+            add_new_item()
+            break
+        elif choise == '2':
+            print("View all item...\n")
+            show_all_item()
+            break
+        elif choise == '3':
+            print(" Delete an item...\n")
+            print("You have to search for the contact you want to delete\n")
+            search_item()
+            break
+        elif choise == '4':
+            print("Search menu...\n")
+            search_item()
+            break
+        elif choise == '5':
+            print("Reset items...\n")
+            validate_reset()
+            break
+        elif choise == '6':
+            print("Exit programme...")
+            exit_programme()
+            break
+        else:
+            print("Not a valid input please enter a number 1-6")
+
+
 def get_sales_data():
     """
     Get sales figures input from the user.
